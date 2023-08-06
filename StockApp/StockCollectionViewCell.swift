caa5520313dd05054b4b036ff1454d8cb968c794
nathanjeong <95894646+NathanJeong@users.noreply.github.com>
2023-08-07 00:01:21 +0900

    #4. collectionView, collectionViewCell, datasource 연결 방법

---
+++ StockApp/StockCollectionViewCell.swift
@@ -0,0 +1,12 @@
+//
+//  StockCollectionViewCell.swift
+//  StockApp
+//
+//  Created by 정동현 on 2023/08/06.
+//
+
+import UIKit
+
+class StockCollectionViewCell: UICollectionViewCell {
+    
+}
--- StockApp/ViewController.swift
+++ StockApp/ViewController.swift
@@ -7,15 +7,33 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, UICollectionViewDelegate {
 
     @IBOutlet weak var collectionView: UICollectionView!
+
+    let stockList: [StockModel] = StockModel.list
 
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        collectionView.dataSource = self
+        collectionView.delegate = self
+    }
+}
+
+extension ViewController: UICollectionViewDataSource {
+    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        return stockList.count
     }
 
+    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        let cellData = collectionView.dequeueReusableCell(withReuseIdentifier: "StockCollectionViewCell", for: indexPath)
+        //return UICollectionViewCell()
+        return cellData
+    }
+}
+
+extension ViewController: UICollectionViewDelegateFlowLayout {
 
 }
 
