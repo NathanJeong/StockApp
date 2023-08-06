a2c0e50c7303e6ee6efec5dc4ace2603d02afe15
nathanjeong <95894646+NathanJeong@users.noreply.github.com>
2023-08-06 23:07:25 +0900

    Initial Commit

---
+++ StockApp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  StockApp
+//
+//  Created by 정동현 on 2023/08/06.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
